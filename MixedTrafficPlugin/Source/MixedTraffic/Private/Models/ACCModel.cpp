e36c4413674237507add230ec04452dcc74359ca
QLwin <qlwin@github.com>
2026-06-12 14:16:04 +0800

    Remove MixedTrafficPlugin (migrated to QLwin/-DSMixTraffic)

--- MixedTrafficPlugin/Source/MixedTraffic/Private/Models/ACCModel.cpp
+++
@@ -1,3 +0,0 @@
-// Copyright QLwin. All Rights Reserved.
-// ACCModel.cpp – all methods are FORCEINLINE in the header.
-#include "Models/ACCModel.h"
--- MixedTrafficPlugin/Source/MixedTraffic/Private/Models/IDMModel.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright QLwin. All Rights Reserved.
-// IDMModel.cpp – implementation file (no inline logic; all in header).
-#include "Models/IDMModel.h"
-
-// All methods are FORCEINLINE in the header.
-// This translation unit exists so the module can export the struct symbol.
